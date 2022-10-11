5a47d788dbd2e5ee3316c94da2629e2cf31f4459
a <yhma22.alkeal@folkuniversitetet.nu>
2022-10-11 12:50:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+     Map <String, String> encryptions = new HashMap<>();
+
+     encryptions.put("Greta", "543 3431 1533");
+     encryptions.put("Karin", "634 2341 2344");
+     encryptions.put("Kevin", "324 5430 1222");
+
+     
+
+
+
+    }
+}
